6d43798b7b49fcbba54edfceb49f2fb35e638c25
ivancornut <iv.cornut@gmail.com>
2016-07-29 11:48:46 -0400

    added in-line arguments

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "simulation.h"
+#include "mt64.h"
 
 int main(int argc, char *argv[])
 {
 	
-	init_genrad64(time(NULL));
+	init_genrand64(time(NULL));
 
 	int x_size = atoi(argv[1]);
 	int y_size = atoi(argv[2]);
-	int iterations = atoi(arg[3]);
+	int iterations = atoi(argv[3]);
 	
 	double wake_up_rate = strtod(argv[4], NULL);
 	double division_rate = strtod(argv[5], NULL);
 	double death_rate = strtod(argv[6], NULL);
-
-	long *landscape = create_landscape();
-	status = run_simulation(x_size, y_size, iterations, landscape, wake_up_rate, division_rate, death_rate);
+	double occupancy = strtod(argv[7], NULL);
+	int status;
+	status = run_simulation(x_size, y_size, iterations, wake_up_rate, division_rate, death_rate, occupancy);
 	if (status == 1)
 	{
 		printf("Everything has worked fine. The csv has been outputed\n");
